22e9e2767ae5ef78e32c28d561bcd23d370f32aa
Samel <samueldeoliveirapereira1@gmail.com>
2022-03-04 10:53:22 -0300

    TipoUsuario feito

---
+++ BackEnd/API/move_ofertas.webAPI/move_ofertas.webAPI/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using move_ofertas.webAPI.Contexts;
+using move_ofertas.webAPI.Domains;
+using move_ofertas.webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace move_ofertas.webAPI.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+        MoveOfertasContext ctx = new MoveOfertasContext();
+        public List<Tipousuario> ListarTodos()
+        {
+            return ctx.Tipousuarios.ToList();
+        }
+    }
+}
